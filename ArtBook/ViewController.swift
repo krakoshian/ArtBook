dd6a53f806c90fd53040d34c5017082e81cdb5a0
Kadir Uludağ <kadiruludag@kadir-macbook-pro.local>
2019-01-01 22:57:00 +0300

    Initial Commit

---
+++ ArtBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtBook
+//
+//  Created by Kadir Uludağ on 1.01.2019.
+//  Copyright © 2019 Kadir Uludağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
